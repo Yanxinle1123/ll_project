c425d0e1a344d9c47227f0d7a17813d749d690c2
yanxinle <1020121123@qq.com>
2024-04-09 20:03:43 +0800

    更新

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
 
+from . import views
+
 """为Django应用程序accounts定义URL模式"""
 
 app_name = 'accounts'
 urlpatterns = [
-    # 用户登录
-    path('', include('django.contrib.auth.urls'))
+    # 包含默认的身份验证URL
+    path('', include('django.contrib.auth.urls')),
+    # 注册界面
+    path('register/', views.register, name='register'),
 ]
