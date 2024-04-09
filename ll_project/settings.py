a784bb314abbe5562af1c5c68cbcae67601eb6e7
yanxinle <1020121123@qq.com>
2024-04-09 18:03:40 +0800

    添加设置

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 我的设置
+LOGIN_REDIRECT_URL = 'learning_logs:index'
