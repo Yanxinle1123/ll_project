a5acdb2fe29c9f6f31d85d66cfef8b7c65fbc48c
yanxinle <1020121123@qq.com>
2024-04-10 13:23:42 +0800

    更新: 保护用户的主题

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.shortcuts import render, redirect
 
 from .forms import TopicForm, EntryForm
@@ -13,7 +14,7 @@ def index(request):
 @login_required
 def topics(request):
     """显示所有的主题"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
@@ -22,6 +23,9 @@ def topics(request):
 def topic(request, topic_id):
     """显示单个主题及其所有条目"""
     topic = Topic.objects.get(id=topic_id)
+    # 确认请求的主题属于当前用户
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -70,6 +74,8 @@ def edit_entry(request, entry_id):
     """编辑既有条目"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
     if request.method != 'POST':
         # 初次请求, 使用当前条目填充表单
         form = EntryForm(instance=entry)
